11f492a2497691ff08602a89f90a8278ca002cdd
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2021-11-17 15:19:08 -0300

    Att header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer/Footer';
-import Produtos from './Components/Produtos';
 import Contato from './Components/Contato';
 import Marcas from './Components/Marcas';
 import CriarConta from './Components/Login/CriarConta';
+import Home from './Components/Home';
 
 const App = () => {
   return (
@@ -16,7 +16,7 @@ const App = () => {
 
         <div className="content">
           <Routes>
-            <Route path="/" element={<Produtos />} end/>
+            <Route path="/" element={<Home />} end/>
             <Route path="marcas" element={<Marcas />} />
             <Route path="contato" element={<Contato />} />
             <Route path="criarConta" element={<CriarConta />} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,22 +3,23 @@ import { NavLink, Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import { ReactComponent as Logo } from '../Assets/img/logo.svg';
 import UseMedia from './Hooks/UseMedia';
+import { useLocation } from 'react-router';
 
 
 const Header = () => {
   const mobile = UseMedia('(max-width: 40rem)')
   const [mobileMenu, setMobileMenu] = React.useState(false);
-console.log(mobile)
 
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname])
 
   return (
     <header className={styles.header}>
-      
-      
       <nav className={`${styles.nav1} container`}> 
-
-      
-        <Link className={`${mobile ? styles.containerMenuMobile : styles.logo}`} to="/" arial-label="Logo - Home" end>
+        <div className={`${mobile ? styles.containerMenuMobile : styles.logo}`} to="/" arial-label="Logo - Home">
           <Logo />
           {mobile &&
             <button 
@@ -27,20 +28,15 @@ console.log(mobile)
               onClick={() => setMobileMenu(!mobileMenu)}>  
             </button>
           }
-        </Link> 
+        </div> 
 
         <ul className={`${mobile ? styles.navMobile : styles.nav1} ${mobileMenu && styles.navMobileActive}`}>
-          <NavLink className={styles.link} to="/" end>Home</NavLink>
+          <NavLink className={styles.link} to="/">Home</NavLink>
           <NavLink className={styles.link} to="marcas">Marcas</NavLink>
           <NavLink className={styles.link} to="contato">Contato</NavLink> 
           <NavLink className={`${styles.linkC}`} to="criarConta">Criar Conta</NavLink>
         </ul>
       </nav>
-      <div className={styles.toggle}>
-        <div className={styles.toggleLine}></div>
-        <div className={styles.toggleLine}></div>
-        <div className={styles.toggleLine}></div>
-      </div>
   </header>
   )
 }
