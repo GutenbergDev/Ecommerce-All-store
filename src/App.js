799e9b0474b3ca1da3b518c133163fe11911ebf8
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2021-11-17 13:28:30 -0300

    Adicionado estilos header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 import Produtos from './Components/Produtos';
 import Contato from './Components/Contato';
 import Marcas from './Components/Marcas';
-import CriarConta from './Components/CriarConta';
+import CriarConta from './Components/Login/CriarConta';
 
 const App = () => {
   return (
@@ -16,7 +16,7 @@ const App = () => {
 
         <div className="content">
           <Routes>
-            <Route path="/" element={<Produtos />} />
+            <Route path="/" element={<Produtos />} end/>
             <Route path="marcas" element={<Marcas />} />
             <Route path="contato" element={<Contato />} />
             <Route path="criarConta" element={<CriarConta />} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,35 +1,49 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import styles from './Header.module.css';
-import logo from '../img/logo/logo.svg';
+import { ReactComponent as Logo } from '../Assets/img/logo.svg';
+import UseMedia from './Hooks/UseMedia';
 
 
 const Header = () => {
+  const mobile = UseMedia('(max-width: 40rem)')
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+console.log(mobile)
+
+
   return (
-    <section className={styles.container}>
-      <div className={styles.header}>
-      <div className={styles.logo}>
-        <NavLink to="/" end><img src={logo} /></NavLink> 
-      </div>
-      <nav className={`${styles.nav}`}> 
+    <header className={styles.header}>
+      
+      
+      <nav className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive} container`}> 
+
+      
+        <Link className={`${mobile ? styles.containerMenuMobile : styles.logo}`} to="/" arial-label="Logo - Home" end>
+          <Logo />
+          {mobile &&
+            <button 
+              aria-label="Menu" 
+              className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+              onClick={() => setMobileMenu(!mobileMenu)}>  
+            </button>
+          }
+        </Link> 
+
         <ul>
-          <li>
-            <NavLink className={styles.link} to="/" end>Home</NavLink>
-          </li>
-          <li>
-            <NavLink className={styles.link} to="marcas">Marcas</NavLink>
-          </li>
-          <li>
-            <NavLink className={styles.link} to="contato">Contato</NavLink>
-          </li>
-          <li>
-            <NavLink className={`${styles.linkC}`} to="criarConta">Criar Conta</NavLink>
-          </li>
+          <NavLink className={styles.link} to="/" end>Home</NavLink>
+          <NavLink className={styles.link} to="marcas">Marcas</NavLink>
+          <NavLink className={styles.link} to="contato">Contato</NavLink> 
+          <NavLink className={`${styles.linkC}`} to="criarConta">Criar Conta</NavLink>
+
+          
         </ul>
-        
       </nav>
+      <div className={styles.toggle}>
+        <div className={styles.toggleLine}></div>
+        <div className={styles.toggleLine}></div>
+        <div className={styles.toggleLine}></div>
       </div>
-    </section>
+  </header>
   )
 }
 
