afc93bb7c4e7951218491a392f282036ae7cd797
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2021-11-29 16:02:43 -0300

    att button

--- src/Components/Home/Main/HomeContent.js
+++ src/Components/Home/Main/HomeContent.js
@@ -20,21 +20,29 @@ const HomeContent = ({ camisetas }) => {
     <section className={styles.container}>
       <div className={styles.mainContent}>
        <div className={styles.mainContainer1}>
-         <div className={styles.mainContainer1Item}>
-            <h3>{camiseta[0].name}</h3>
-            <p>{camiseta[0].description}</p>
-         </div>
-         <div className={styles.mainContainer1Item2}>
-           <div>
-            <img src={camiseta[0].image[0]} alt="" />
-           </div>
-           <div className={styles.containerPrice}>
-            <div className={styles.priceStyle}>
-              <span className={styles.price}>R$ {camiseta[0].price}</span>
+         <div style={{ display: 'flex' }}>
+          <div className={styles.mainContainer1Item}>
+              <h3>{camiseta[0].name}</h3>
+              <p>{camiseta[0].description}</p>
+              
+          </div>
+          <div className={styles.mainContainer1Item2}>
+            <div>
+              <img src={camiseta[0].image[0]} alt="" />
             </div>
-            <span><span className={styles.oldPrice}>R$ {(camiseta[0].OldPrice).toFixed(2)}</span> ou 12x de <span className={styles.parcelas}>R$ {(camiseta[0].OldPrice / 12).toFixed(2)}</span></span>
+            <div className={styles.containerPrice}>
+              <div className={styles.priceStyle}>
+                <span className={styles.price}>R$ {camiseta[0].price}</span>
+              </div>
+              <span><span className={styles.oldPrice}>R$ {(camiseta[0].OldPrice).toFixed(2)}</span> ou 12x de <span className={styles.parcelas}>R$ {(camiseta[0].OldPrice / 12).toFixed(2)}</span></span>
+            </div>
           </div>
          </div>
+         <div className={styles.btnContainer}>
+            <button className={styles.btnMain}>
+              <span className={styles.btnMainText}>Ver Mais</span>
+            </button>
+          </div>
        </div>
        <div className={styles.mainContainer2}>
           bb
